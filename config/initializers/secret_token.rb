5020b0ba21e730b57f5d322fbcebe74bc8db8061
Michael Ferraco <ferraco.mike@gmail.com>
2014-05-20 22:37:55 -0400

    initial rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorldCupPoll::Application.config.secret_key_base = '1074e03993eba15a2a9844eb5b09e908b89c17a4360d6e14d31817f9ff6845ba670f438e3d8ce96862b754e42221b73abfcf15161ad71686e1d78e7fe01353a7'
